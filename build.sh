63c948b665ef048351210a493ed2bc93482df155
holzmanj <jesseholzman96@gmail.com>
2020-01-31 19:22:43 -0500

    Update build script for new structure

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-shopt -s nullglob
+set -e
 
-# transpile elm code
-elm make src/Main.elm
+SOURCE_DIRS=("Environment" "Applications/Lifelike")
 
-# inject links to css files
-for f in style/*.css
-do
-    sed -i "/<head>/ a <link href='$f' rel='stylesheet' type='text/css'>" index.html
+elm_to_js() {
+    # Args
+    #   $1 - path to elm source file
+    #   $2 - path to js output file
+    tmp="tmp.js"
+    echo "Transpiling elm code..."
+    elm make --optimize --output=$tmp $1
+    echo "Compressing js to $2..."
+    uglifyjs $tmp --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=$2
+    echo "Done! (final size: $(stat -c %s $2 | numfmt --to=si))"
+    rm $tmp
+}
+
+base_dir=$(pwd)
+
+for dir in "${SOURCE_DIRS[@]}"; do
+    cd $dir
+    elm_to_js src/Main.elm "${base_dir}/scripts/$(basename $dir).min.js"
+    cd $base_dir
 done
